2c58daeb55b4d31cb3219c679ecf238e70513ce4
Giovanni <agf_2000@hotmail.com>
2018-09-24 23:10:06 -0300

    Fininshing CCEC

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('index', {
-		title: 'Colégio CEC :: Home',
-		header: '/views/partials/homeHeader.hbs'
+		title: 'Colégio CEC :: Home'
+	});
+});
+
+/* GET home page. */
+router.get('/contatos', function (req, res, next) {
+	res.render('contacts', {
+		title: 'Colégio CEC :: Contatos',
+		layout: 'pages'
 	});
 });
 
